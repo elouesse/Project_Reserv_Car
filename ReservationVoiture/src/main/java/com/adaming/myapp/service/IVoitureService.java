5c16223ab045e96c37429f98b1a2d3eb57705bcc
Eli <elouesse@hotmail.com>
2016-08-09 15:36:48 +0200

    commit service Voiture

--- ReservationVoiture/src/main/java/com/adaming/myapp/service/IVoitureService.java
+++ ReservationVoiture/src/main/java/com/adaming/myapp/service/IVoitureService.java
@@ -1,5 +1,20 @@
 package com.adaming.myapp.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.adaming.myapp.entities.Voiture;
+
 public interface IVoitureService {
+	
+	public Voiture addVoiture(Voiture c);
+	public Voiture getVoiture(Long id);
+	public Voiture updateVoiture(Voiture c);
+	public Voiture deleteVoiture(Long id);
+	public List<Voiture> getVoitures();
+	public List<Voiture> getVoituresDisp();
+	public List<Voiture> getVoituresDispByPeriod(Date d1, Date d2);
+	public void alertEntretien(Long idV, Long idE,Double pref);
+	public List<Voiture> retourVoitures();
 
 }
--- ReservationVoiture/src/main/java/com/adaming/myapp/service/VoitureServiceImpl.java
+++ ReservationVoiture/src/main/java/com/adaming/myapp/service/VoitureServiceImpl.java
@@ -1,5 +1,70 @@
 package com.adaming.myapp.service;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.adaming.myapp.dao.IVoitureDao;
+import com.adaming.myapp.entities.Voiture;
+
+@Service
+@Transactional
 public class VoitureServiceImpl implements IVoitureService {
+	
+	@Autowired
+	private IVoitureDao daoVoiture;
+
+	public void setDaoVoiture(IVoitureDao daoVoiture) {
+		this.daoVoiture = daoVoiture;
+	}
+
+	@Override
+	public Voiture addVoiture(Voiture c) {
+		return daoVoiture.addVoiture(c);
+	}
+
+	@Override
+	public Voiture getVoiture(Long id) {
+		return daoVoiture.getVoiture(id);
+	}
+
+	@Override
+	public Voiture updateVoiture(Voiture c) {
+		return daoVoiture.updateVoiture(c);
+	}
+
+	@Override
+	public Voiture deleteVoiture(Long id) {
+		return daoVoiture.deleteVoiture(id);
+	}
+
+	@Override
+	public List<Voiture> getVoitures() {
+		return daoVoiture.getVoitures();
+	}
+
+	@Override
+	public List<Voiture> getVoituresDisp() {
+		return daoVoiture.getVoituresDisp();
+	}
+
+	@Override
+	public List<Voiture> getVoituresDispByPeriod(Date d1, Date d2) {
+		return daoVoiture.getVoituresDispByPeriod(d1, d2);
+	}
+
+	@Override
+	public void alertEntretien(Long idV, Long idE, Double pref) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Voiture> retourVoitures() {
+		return daoVoiture.retourVoitures();
+	}
 
 }
