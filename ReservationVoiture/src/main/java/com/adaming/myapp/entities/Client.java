798cd0798cde970def5581d77f2fb4ba23957e64
Eli <elouesse@hotmail.com>
2016-08-09 10:20:33 +0200

    commit entites

--- ReservationVoiture/src/main/java/com/adaming/myapp/entities/Client.java
+++ ReservationVoiture/src/main/java/com/adaming/myapp/entities/Client.java
@@ -3,10 +3,138 @@
  */
 package com.adaming.myapp.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  * @author Eli
  *
  */
+@Entity
 public class Client {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idClient;
+	private String nom;
+	private String prenom; 
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date   dateDeNaissance; 
+	private String numeroTel; 
+	private String numeroPermis; 
+	private String adressPostal; 
+	private String mail; 
+	private String pays; 
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date   dateDeDelivrance;
+	private String lieuDeNaissance;
+	private String delivrerPar;
+	
+	public Client(String nom, String prenom, Date dateDeNaissance,
+			String numeroTel, String numeroPermis, String adressPostal,
+			String mail, String pays, Date dateDeDelivrance,
+			String lieuDeNaissance, String delivrerPar) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateDeNaissance = dateDeNaissance;
+		this.numeroTel = numeroTel;
+		this.numeroPermis = numeroPermis;
+		this.adressPostal = adressPostal;
+		this.mail = mail;
+		this.pays = pays;
+		this.dateDeDelivrance = dateDeDelivrance;
+		this.lieuDeNaissance = lieuDeNaissance;
+		this.delivrerPar = delivrerPar;
+	}
+	
+	
+	public Long getIdClient() {
+		return idClient;
+	}
+	public void setIdClient(Long idClient) {
+		this.idClient = idClient;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateDeNaissance() {
+		return dateDeNaissance;
+	}
+	public void setDateDeNaissance(Date dateDeNaissance) {
+		this.dateDeNaissance = dateDeNaissance;
+	}
+	public String getNumeroTel() {
+		return numeroTel;
+	}
+	public void setNumeroTel(String numeroTel) {
+		this.numeroTel = numeroTel;
+	}
+	public String getNumeroPermis() {
+		return numeroPermis;
+	}
+	public void setNumeroPermis(String numeroPermis) {
+		this.numeroPermis = numeroPermis;
+	}
+	public String getAdressPostal() {
+		return adressPostal;
+	}
+	public void setAdressPostal(String adressPostal) {
+		this.adressPostal = adressPostal;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getPays() {
+		return pays;
+	}
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	public Date getDateDeDelivrance() {
+		return dateDeDelivrance;
+	}
+	public void setDateDeDelivrance(Date dateDeDelivrance) {
+		this.dateDeDelivrance = dateDeDelivrance;
+	}
+	public String getLieuDeNaissance() {
+		return lieuDeNaissance;
+	}
+	public void setLieuDeNaissance(String lieuDeNaissance) {
+		this.lieuDeNaissance = lieuDeNaissance;
+	}
+	public String getDelivrerPar() {
+		return delivrerPar;
+	}
+	public void setDelivrerPar(String delivrerPar) {
+		this.delivrerPar = delivrerPar;
+	}
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 
 }
--- ReservationVoiture/src/main/java/com/adaming/myapp/entities/Reservation.java
+++ ReservationVoiture/src/main/java/com/adaming/myapp/entities/Reservation.java
@@ -3,10 +3,139 @@
  */
 package com.adaming.myapp.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  * @author Eli
  *
  */
+@Entity
 public class Reservation {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idreservation;
+	private double prix;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date dateDeReservation;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date dateDeRentrer;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	private Date dateDeSortie;
+	private String heureDeSortie;
+	private String heureDeRentrer;
+	private Integer nombresDeJours;
+	private String etatDeReservation;
+	
+	//Association
+	@ManyToOne
+    private Client client;
+    @ManyToOne
+    private Voiture voiture;
+    
+    //Constructors
+    public Reservation() {
+		super();
+	}
+	
+	public Reservation(double prix, Date dateDeReservation, Date dateDeRentrer,
+			Date dateDeSortie, String heureDeSortie, String heureDeRentrer,
+			Integer nombresDeJours, String etatDeReservation) {
+		super();
+		this.prix = prix;
+		this.dateDeReservation = dateDeReservation;
+		this.dateDeRentrer = dateDeRentrer;
+		this.dateDeSortie = dateDeSortie;
+		this.heureDeSortie = heureDeSortie;
+		this.heureDeRentrer = heureDeRentrer;
+		this.nombresDeJours = nombresDeJours;
+		this.etatDeReservation = etatDeReservation;
+	}
+	
+	
+	public Long getIdreservation() {
+		return idreservation;
+	}
+	public void setIdreservation(Long idreservation) {
+		this.idreservation = idreservation;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public Date getDateDeReservation() {
+		return dateDeReservation;
+	}
+	public void setDateDeReservation(Date dateDeReservation) {
+		this.dateDeReservation = dateDeReservation;
+	}
+	public Date getDateDeRentrer() {
+		return dateDeRentrer;
+	}
+	public void setDateDeRentrer(Date dateDeRentrer) {
+		this.dateDeRentrer = dateDeRentrer;
+	}
+	public Date getDateDeSortie() {
+		return dateDeSortie;
+	}
+	public void setDateDeSortie(Date dateDeSortie) {
+		this.dateDeSortie = dateDeSortie;
+	}
+	public String getHeureDeSortie() {
+		return heureDeSortie;
+	}
+	public void setHeureDeSortie(String heureDeSortie) {
+		this.heureDeSortie = heureDeSortie;
+	}
+	public String getHeureDeRentrer() {
+		return heureDeRentrer;
+	}
+	public void setHeureDeRentrer(String heureDeRentrer) {
+		this.heureDeRentrer = heureDeRentrer;
+	}
+	public Integer getNombresDeJours() {
+		return nombresDeJours;
+	}
+	public void setNombresDeJours(Integer nombresDeJours) {
+		this.nombresDeJours = nombresDeJours;
+	}
+	public String getEtatDeReservation() {
+		return etatDeReservation;
+	}
+	public void setEtatDeReservation(String etatDeReservation) {
+		this.etatDeReservation = etatDeReservation;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public Voiture getVoiture() {
+		return voiture;
+	}
+	public void setVoiture(Voiture voiture) {
+		this.voiture = voiture;
+	}
+	
+	
+	
+
 
 }
